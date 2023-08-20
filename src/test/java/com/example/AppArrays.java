0ad3ad6950d22a8d1cfb66729c06bda0a5ce4566
Andrelfz Lopes <andrelts.teixeira@gmail.com>
2023-08-20 03:05:35 +0000

    Criação de arrays

---
+++ src/test/java/com/example/AppArrays.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArrays {
+    public static void main(String[] args) {
+        double vetor[] = {10,20,30};
+        System.out.println(vetor [1]);
+
+
+    String vetorNomes[] = {"Banana", "Maçã", "Laranja"};
+        System.out.println(vetorNomes[2]);
+
+        int vetorNovo [] = new int[3];
+        vetorNovo[0] = 200;
+        vetorNovo[1] = 300;
+        vetorNovo[2] = 400;
+        vetorNovo[3] = 500;
+
+        System.out.println(vetorNovo[3]);
+
+    int Matriz [][] = new int [2][2];
+    Matriz [0][0] = 100;
+    Matriz [0][1] = 200;
+    System.out.println(Matriz[0][1]);
+    }
+    
+}
+
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,11 +4,20 @@ package com.example;
 
 public class AppTest {
 
+    /**
+     * @param args
+     */
     public static void main (String[] args){
        String a = ("oi eu sou goku");
 
         System.out.println(a);
 
+        int b = 3;
+        int c = 4;
+        double soma = b + c;
+    
+        
     }
+
 
 }
